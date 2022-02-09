6b708daa984ae460d065f7561528701a28bc4ad6
Victor <victorssantos572@gmail.com>
2022-02-09 16:02:32 -0300

    More animations and colisions

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 var velocity = Vector2.ZERO
-var move_speed = 480
+var move_speed = 100
 var gravity = 1200
 var jump_force = -720
 
@@ -9,8 +9,8 @@ func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
 	
 	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
-	if Input.is_action_pressed("jump"):
-		velocity.y = jump_force /2
+
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = jump_force /2.5
 	velocity.x = move_speed * move_direction
-	
 	move_and_slide(velocity)
