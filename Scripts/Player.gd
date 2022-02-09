2225e6cd83d24dc89ef186113d1cd36b623ba98a
Victor <victorssantos572@gmail.com>
2022-02-09 16:55:35 -0300

    Jump

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -4,13 +4,33 @@ var velocity = Vector2.ZERO
 var move_speed = 100
 var gravity = 1200
 var jump_force = -720
+var is_grounded
+onready var raycasts = $Raycasts
 
 func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
 	
+	_get_input()
+	
+	velocity = move_and_slide(velocity)
+	
+	is_grounded = _check_is_ground()
+	
+func _get_input():
+	
 	var move_direction = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
-
-	if Input.is_action_just_pressed("jump"):
-		velocity.y = jump_force /2.5
 	velocity.x = move_speed * move_direction
-	move_and_slide(velocity)
+	
+	if move_direction !=0:
+		scale.x = move_direction
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("jump") and is_grounded:
+		velocity.y = jump_force /2
+
+func _check_is_ground():
+	for raycast in raycasts.get_children():
+		if raycast.is_colliding():
+			return true
+			
+	return false
