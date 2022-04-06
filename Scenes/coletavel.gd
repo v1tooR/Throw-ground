49b1ead0e6d5c5776e68c3503ae22bfc4002ffbf
Guilherme Samuel <guisamuel800@gmail.com>
2022-04-06 14:45:54 -0300

    adição de coletável

--- Scenes/coletavel.gd
+++ Scenes/coletavel.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
+onready var anim = $AnimatedSprite
+ 
+func _on_coletavel_body_entered(body) -> void:
+	if body.is_in_group("player"):
+		anim.play("coletado")
 
-func _on_coletavel_body_entered(body: Node) -> void:
-	$Animate.play("collected")
-
-func _on_AnimatedSprite_animation_finished(Animate_name: String) -> void:
-	if Animate_name == "collected":
+func _on_AnimatedSprite_animation_finished() -> void:
+	if anim.get_animation() == "coletado":
 		queue_free()
